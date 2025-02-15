9537f32435b6053532a1032b987063d6968909c7
Simpul Simon <simpulsimonrout@gmail.com>
2025-02-15 12:38:55 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup ,find_packages
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Simpul Simon',
+    author_email='simpulsimonrout@gmail.com',
+    packages= find_packages(),
+    install_requires = [],
+)
